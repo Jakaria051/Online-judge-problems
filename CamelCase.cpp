3f18cd0a5bfef231ecde58da9d5dffd4ea0ff27f
Jakaria Ahmed Chowdhury <32171981+Jakaria051@users.noreply.github.com>
2018-02-04 03:14:41 +0600

    C++

---
+++ CamelCase.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //vector <int> input;
+    //vector <int> :: iterator it;
+    int len,count=1;
+    string s;
+    cin>>s;
+    len = s.length();
+
+    /*for(int i=0;i<len;i++)
+    {
+        //int j;
+        //cin>>j;
+
+        input.push_back(i);
+
+    }*/
+
+    for(int i=0;i<len;i++)
+    {
+        if(s[i]>=65 && s[i]<=90)
+            count++;
+    }
+    cout<<count<<endl;
+
+}
